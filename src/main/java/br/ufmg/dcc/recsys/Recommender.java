93d22778785a4e0e9d3675611a2cb6b3b06ee960
Aécio Santos <aecio.solando@gmail.com>
2014-09-02 02:06:08 -0300

    Added evaluation using RMSE

--- src/main/java/br/ufmg/dcc/recsys/Recommender.java
+++ src/main/java/br/ufmg/dcc/recsys/Recommender.java
@@ -1,56 +1,15 @@
 package br.ufmg.dcc.recsys;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-
 public class Recommender {
-    public static void main(String[] args) throws IOException {
-        if (args.length != 3) {
-            throw new IllegalArgumentException("Invalid parameters.");
-        }
-        PrintStream ps = new PrintStream("./debug.txt");
-        System.setErr(ps);
-        
-//        String usersFile = args[0];
-//        String itemsFile = args[1];
-        String ratingsFile = args[2];
-
-
-//        List<String> lines = Files.lines(Paths.get(usersFile))
-//                .map((line) -> line.split("\\|")[0])
-//                .collect(Collectors.toList());
-//        
-//        
-//        CharsetDecoder decoder = StandardCharsets.UTF_8.newDecoder();
-//        decoder.onMalformedInput(CodingErrorAction.IGNORE);
-//        InputStream is = new FileInputStream(new File(itemsFile));
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(is, decoder));
-//        
-//        List<String> lines = reader.lines()
-//                .map((line) -> line.split("\\|")[0])
-//                .map((line) -> { System.out.println(line);  return line;})
-//                .collect(Collectors.toList());
-//        reader.close();
-        
-        Matrix ratingsMatrix = readRatingsMatrix(ratingsFile);
-        
-        UserToUserPredictor userToUserPredictor = new UserToUserPredictor(ratingsMatrix, 20);
-        TopkRecommender userToUserRecommender = new TopkRecommender(ratingsMatrix, userToUserPredictor, 100);
-        
-        int numUsers = ratingsMatrix.numRows();
-        
-        IntStream.range(0, numUsers)
-                .mapToObj((u) -> userToUserRecommender.recommend(u))
-                .flatMap((recs) -> recs.stream() )
-                .forEach((p) -> System.out.println((p.user+1) +" "+ (p.item+1) +" "+ p.score) );
-    }
-
-    static class Rating {
+    
+    public static class Rating {
         int userId;
         int itemId;
         int rating;
@@ -64,23 +23,71 @@ public class Recommender {
         }
     }
     
+    public static void main(String[] args) throws IOException {
+        if (args.length < 3) {
+            System.err.println("Usage: java -jar tp1.jar Recommender u.user u.item ua.base");
+            System.exit(1);
+        }
+        
+        String option = args[0];
+        String ratingsFile = args[3];
+        String testFile = args[4];
+
+        Matrix ratingsMatrix = readRatingsMatrix(ratingsFile);
+        
+        if(option.equalsIgnoreCase("Recommender")) {
+            recomendUserToUser(ratingsMatrix);
+        }
+        
+        if(option.equalsIgnoreCase("Evaluation")) {
+            evaluatePredictors(testFile, ratingsMatrix);
+        }
+    }
+
+    private static void recomendUserToUser(Matrix ratingsMatrix) {
+        UserToUserPredictor predictor = new UserToUserPredictor(ratingsMatrix, 20);
+        TopkRecommender recommender = new TopkRecommender(ratingsMatrix, predictor, 100);
+        
+        int numUsers = ratingsMatrix.numRows();
+        
+        IntStream.range(0, numUsers)
+            .mapToObj( u -> recommender.recommend(u))
+            .flatMap( recs -> recs.stream() )
+            .forEach( p -> System.out.println((p.user+1) +" "+ (p.item+1) +" "+ p.score) );
+    }
+
+    private static void evaluatePredictors(String testFile, Matrix ratingsMatrix) {
+        if(testFile != null && !testFile.isEmpty()) {
+            IntStream.of(1, 3, 5, 10, 25, 50, 100, 150, 200).forEach( k -> {
+                UserToUserPredictor predictor = new UserToUserPredictor(ratingsMatrix, k);
+                double rmse = calculateRmse(testFile, predictor);
+                System.out.println("RMSE user-to-user ("+k+"): "+rmse);
+            });
+            double rmse = calculateRmse(testFile, new AverageItemRatingPredictor(ratingsMatrix));
+            System.out.println("RMSE avg-item-rating: "+rmse);
+            System.out.flush();
+        }
+    }
+    
     private static Matrix readRatingsMatrix(String ratingsFile) {
         try {
             List<Rating> ratings = Files.lines(Paths.get(ratingsFile))
-                    .map((line) -> {
+                    .map( line -> {
                         String[] t = line.split("\\t");
-                        return new Rating(Integer.valueOf(t[0]), Integer.valueOf(t[1]), Integer.valueOf(t[2]), Integer.valueOf(t[3]));
+                        return new Rating(Integer.valueOf(t[0]),
+                                          Integer.valueOf(t[1]),
+                                          Integer.valueOf(t[2]),
+                                          Integer.valueOf(t[3]));
                     })
-                   .collect(Collectors.toList());
+                    .collect(Collectors.toList());
             
-            
-            int numberOfUsers = ratings.stream().mapToInt((r)->r.userId).max().getAsInt();
-            int numberOfItems = ratings.stream().mapToInt((r)->r.itemId).max().getAsInt();
+            int numberOfUsers = ratings.stream().mapToInt(r->r.userId).max().getAsInt();
+            int numberOfItems = ratings.stream().mapToInt(r->r.itemId).max().getAsInt();
             
             double matrix[][] = new double[numberOfUsers][numberOfItems];
             
-            ratings.forEach( (r) -> {
-              matrix[r.userId-1][r.itemId-1] = r.rating;  
+            ratings.forEach( r -> {
+                matrix[r.userId-1][r.itemId-1] = r.rating;  
             });
             
             return new Matrix(matrix);
@@ -89,4 +96,28 @@ public class Recommender {
             throw new RuntimeException("Failed to read ratings file: "+ratingsFile);
         }
     }
+
+    private static double calculateRmse(String testFile, Predictor predictor) {
+        try {
+            OptionalDouble squaredError = Files.lines(Paths.get(testFile))
+                    .map( line -> {
+                        String[] t = line.split("\\t");
+                        return new Rating(Integer.valueOf(t[0]),
+                                          Integer.valueOf(t[1]),
+                                          Integer.valueOf(t[2]),
+                                          Integer.valueOf(t[3]));
+                    })
+                    .mapToDouble( r -> {
+                        double p = predictor.predict(r.itemId-1, r.userId-1);
+                        return Math.pow(p - r.rating, 2);
+                    })
+                    .average();
+            
+            return Math.sqrt(squaredError.getAsDouble());
+            
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to run evaluation.", e);
+        }
+    }
+    
 }
